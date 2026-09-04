9445f91f793c1badf7a190e9f69be5784d7ec7ab
dimi2706 <dimitri.manfredonia@ynov.com>
2026-09-04 13:43:33 +0200

    quetes 03

--- my-react-app/src/components/PokemonCard.tsx
+++ my-react-app/src/components/PokemonCard.tsx
@@ -1,14 +1,29 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+
+  const pokemon = pokemonList[0];
+
   return (
     <div className="pokemon-card">
       <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt="Bulbasaur"
-        />
+        {pokemon.imgSrc !== undefined ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+        )}
       </figure>
       <figcaption>
-        <h1>Bulbisard</h1>
+        <h1>{pokemon.name}</h1>
       </figcaption>
     </div>
   );
